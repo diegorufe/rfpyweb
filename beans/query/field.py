c7b407661eb5d4d3c557a3ed6fdc28074c9198cb
diegorufe <diegorufe@gmail.com>
2020-06-19 06:50:59 +0200

    Updated field and erase content for readme

--- beans/query/field.py
+++ beans/query/field.py
@@ -1,12 +1,14 @@
 class Field:
 
-    def __init__(self, name=None, alias_table=None, alias_field=None):
+    def __init__(self, name=None, alias_table=None, alias_field=None, custom_field=None):
         """
         Constructor for class field
         :param name: is the name for field
         :param alias_table: for get the field
         :param alias_field: for field
+        :pmara custom_field: for field ignore name, alias ...
         """
         self.name = name
         self.alias_table = alias_table
         self.alias_field = alias_field
+        self.custom_field = custom_field
